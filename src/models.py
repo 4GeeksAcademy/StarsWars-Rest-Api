ca54a8ddc7a1a4336c5bb2d6b881d6efa3897c05
Kevin Villafuerte Montero <116960904+Kevin962022@users.noreply.github.com>
2024-09-03 21:20:03 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -1,19 +1,56 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(db.Model): 
+    __tablename__ = 'user'   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    contrasenia = Column(String(250), nullable=False)
+    fecha_suscrip = Column(DateTime, nullable=False, default=datetime.utcnow)
+    # Relación uno a muchos con Favorite
+    favorites = relationship('Favorite', back_populates='user')
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Character(db.Model): 
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
-    def serialize(self):
+class Planet(db.Model):  
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Vehicle(db.Model):  
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Favorite(db.Model):  
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='favorites')
+    
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship('Character')
+    
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship('Planet')
+    
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    vehicle = relationship('Vehicle')
+
+    def to_dict(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
+            "vehicle_id": self.vehicle_id,
         }
